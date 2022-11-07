4d2502085b0110c59cbced007fe225532a14341e
unknown <you@example.com>
2022-11-07 10:29:12 +0600

    PRODUCT

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ProductsList from "./pages/ProductsList";
 function App() {
     return (
         <div className="App">
-            <Cart/>
+            <Product/>
 
         </div>
     );
